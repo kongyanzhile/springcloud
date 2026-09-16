8acbd0b8c7dd46de5ee1ed873cad3b28c4d660bf
xian-qinzx <xa_qinzx@sunseer.co.jp>
2026-09-16 17:14:23 +0800

    elasticsearch

---
+++ springcloud-search/src/main/java/com/example/springcloudsearch/SpringcloudSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudSearchApplication.class, args);
+    }
+
+}
---
+++ springcloud-search/src/test/java/com/example/springcloudsearch/SpringcloudSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
