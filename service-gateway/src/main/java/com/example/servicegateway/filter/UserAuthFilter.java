534d2574747c86e19b8312e2a10dc998d3709c19
xian-qinzx <xa_qinzx@sunseer.co.jp>
2026-09-18 16:37:22 +0800

    访问不需要权限的url，如果登陆了，把登录用户id也透传过去

--- service-gateway/src/main/java/com/example/servicegateway/filter/UserAuthFilter.java
+++ service-gateway/src/main/java/com/example/servicegateway/filter/UserAuthFilter.java
@@ -1,25 +1,19 @@
 package com.example.servicegateway.filter;
 
 import com.example.servicegateway.properties.AuthUrlProperties;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.env.Environment;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.lang.annotation.Annotation;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * UserAuthFilter<br>
@@ -41,11 +35,25 @@ public class UserAuthFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String path = exchange.getRequest().getURI().getPath();
+
         // 不需要权限的
         List<String> noauthurlList = authUrlProperties.getNoauthurl();
         for (String noauthurl : noauthurlList) {
-            String path = exchange.getRequest().getURI().getPath();
             if (antPathMatcher.match(noauthurl, path)) {
+                String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+                if (StringUtils.hasText(token)){ // 如果在登录的情况下访问了不需要登录的url，把token也发送过去
+                    String[] split = token.split(" ");
+                    if (split.length == 2 && "Bearer".equalsIgnoreCase(split[0])) {
+                        String userId = redisTemplate.opsForValue().get("login:token:" + split[1]);
+                        if (StringUtils.hasText(userId)) {
+                            exchange = exchange.mutate()
+                                    .request(builder -> builder.header("X-User-Id", userId))
+                                    .build();
+                        }
+                    }
+                }
                 return chain.filter(exchange);
             }
         }
@@ -53,23 +61,36 @@ public class UserAuthFilter implements GlobalFilter, Ordered {
         // 需要权限的
         List<String> authurlList = authUrlProperties.getAuthurl();
         for (String authurl : authurlList) {
-            String path = exchange.getRequest().getURI().getPath();
             if (antPathMatcher.match(authurl, path)) {
                 String token = exchange.getRequest().getHeaders().getFirst("Authorization");
-                String[] split = token.split(" ");
-                String userId = redisTemplate.opsForValue().get("login:token:" + split[1]);
-                if (!Objects.isNull(userId)) {
-                    System.out.println("[网关] 即将设置 X-User-Id = " + userId);
-                    ServerWebExchange mutatedExchange = exchange.mutate()
-                            .request(builder -> builder.header("X-User-Id", userId))
-                            .build();
-                    System.out.println("[网关-转发前验证] X-User-Id = "
-                            + mutatedExchange.getRequest().getHeaders().getFirst("X-User-Id"));
-
-                    return chain.filter(mutatedExchange);
+                if (StringUtils.hasText(token)){
+                    String[] split = token.split(" ");
+                    if (split.length != 2 || !"Bearer".equalsIgnoreCase(split[0])) {
+                        return this.unauthorized(exchange);
+                    }
+                    String userId = redisTemplate.opsForValue().get("login:token:" + split[1]);
+                    if (StringUtils.hasText(userId)) {
+                        ServerWebExchange mutatedExchange = exchange.mutate()
+                                .request(builder -> builder.header("X-User-Id", userId))
+                                .build();
+                        return chain.filter(mutatedExchange);
+                    } else {
+                        return this.unauthorized(exchange);
+                    }
+                } else {
+                    return this.unauthorized(exchange);
                 }
             }
         }
+        return this.unauthorized(exchange);
+    }
+
+    /**
+     * 返回无权限状态
+     * @param exchange
+     * @return
+     */
+    private Mono<Void> unauthorized(ServerWebExchange exchange) {
         exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
         return exchange.getResponse().setComplete();
     }
